3795de240b8014315e4170beada88b6468bbf1ae
Myyk Seok <2080820+myyk@users.noreply.github.com>
2025-02-13 17:52:37 +0800

    chore: Manifest V3 and complete rewrite to not have errors

--- content.js
+++ content.js
@@ -1,4 +1,4 @@
-var elements = document.getElementsByTagName('*');
+console.log('Content script loaded');
 
 var sourceWordsToTargetWords = [
     [['bro', 'bruh', 'bruv', 'brah', 'broseph', 'mate'], 'boyfriend'],
@@ -7,56 +7,30 @@ var sourceWordsToTargetWords = [
     [['babe'], 'idk your name (so I\'m going to call you something that supposedly sounds like I\'m complimenting you because I have a favour to ask)'],
 ];
 
-function makeRegex(sourceWords) {
-    return new RegExp('\\b' + sourceWords.join('\\b|\\b') + '\\b', 'g');
-};
-
-function identity(string) {
-    return string;
-};
-
-function capitalizeFirstLetter(string) {
-    return string.charAt(0).toUpperCase() + string.slice(1);
-};
-
-function toUpperCase(string) {
-    return string.toUpperCase();
-};
-
-function makeRegexToTargetWords(sourceWordsToTargetWords, modifyWords) {
-    return sourceWordsToTargetWords.map(function(sourceAndTarget) {
-        var [source,target] = sourceAndTarget;
-        source = source.map(modifyWords);
-        target = modifyWords(target);
-        return [makeRegex(source), target];
-    });
-};
-
-var sourceRegexToTargetWords = makeRegexToTargetWords(sourceWordsToTargetWords, identity).concat(makeRegexToTargetWords(sourceWordsToTargetWords, capitalizeFirstLetter)).concat(makeRegexToTargetWords(sourceWordsToTargetWords, toUpperCase));
-
-function replaceTextWithRegexes(text, sourceRegexToTargetWords) {
-    for (var k = 0; k < sourceRegexToTargetWords.length; k++) {
-        var [regex, targetWord] = sourceRegexToTargetWords[k];
-        var replacedText = text.replace(regex, targetWord);
-        text = replacedText
-    }
-    return text;
-};
-
-for (var i = 0; i < elements.length; i++) {
-    var element = elements[i];
-
-    for (var j = 0; j < element.childNodes.length; j++) {
-        var node = element.childNodes[j];
-
-        if (node.nodeType === 3) {
-            var text = node.nodeValue;
-            replacedText = replaceTextWithRegexes(text, sourceRegexToTargetWords);
-
-            if (replacedText !== text) {
-                console.log('replaced');
-                element.replaceChild(document.createTextNode(replacedText), node);
-            }
-        }
+function replaceText(node) {
+    if (node.nodeType === Node.TEXT_NODE) {
+        let text = node.nodeValue;
+        sourceWordsToTargetWords.forEach(([sourceWords, targetWord]) => {
+            sourceWords.forEach(sourceWord => {
+                let regex = new RegExp(`\\b${sourceWord}\\b`, 'gi');
+                text = text.replace(regex, (match) => {
+                    if (match === match.toUpperCase()) {
+                        return targetWord.toUpperCase();
+                    } else if (match[0] === match[0].toUpperCase()) {
+                        return targetWord.charAt(0).toUpperCase() + targetWord.slice(1);
+                    } else {
+                        return targetWord;
+                    }
+                });
+            });
+        });
+        node.nodeValue = text;
+    } else if (node.nodeType === Node.ELEMENT_NODE && node.nodeName !== 'SCRIPT' && node.nodeName !== 'STYLE') {
+        node.childNodes.forEach(child => replaceText(child));
     }
 }
+
+window.onload = () => {
+    console.log('Window onload event fired');
+    replaceText(document.body);
+};
